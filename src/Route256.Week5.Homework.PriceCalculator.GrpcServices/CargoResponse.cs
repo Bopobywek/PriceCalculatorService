a6487c30419b4b895fa798c9b4773c3fe7e690e3
Andrew N <domiantor@yandex.ru>
2023-04-18 00:18:08 +0300

    [CSJHW-7]: реализовал server-streaming метод для получения истории расчетов

---
+++ src/Route256.Week5.Homework.PriceCalculator.GrpcServices/CargoResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Route256.Week5.Homework.PriceCalculator.GrpcServices;
+
+public partial class CargoResponse
+{
+    public static CargoResponse Create(double volume, double weight, long[] goodIds)
+    {
+        var result = new CargoResponse
+        {
+            Volume = volume,
+            Weight = volume
+        };
+        
+        result.GoodIds.AddRange(goodIds);
+
+        return result;
+    }
+}
--- src/Route256.Week5.Homework.PriceCalculator.GrpcServices/Services/DeliveryPriceCalculatorService.cs
+++ src/Route256.Week5.Homework.PriceCalculator.GrpcServices/Services/DeliveryPriceCalculatorService.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Route256.Week5.Homework.PriceCalculator.Bll.Commands;
 using Route256.Week5.Homework.PriceCalculator.Bll.Models;
+using Route256.Week5.Homework.PriceCalculator.Bll.Queries;
 
 namespace Route256.Week5.Homework.PriceCalculator.GrpcServices.Services;
 
@@ -42,9 +43,38 @@ public class DeliveryPriceCalculatorService : DeliveryPriceCalculator.DeliveryPr
             request.UserId,
             request.CalculationIds.ToArray()
         );
-        
+
         await _mediator.Send(query, context.CancellationToken);
 
         return new ClearHistoryResponse();
+    }
+
+    public override async Task GetHistory(GetHistoryRequest request,
+        IServerStreamWriter<GetHistoryResponse> responseStream,
+        ServerCallContext context)
+    {
+        const int take = 100;
+        int skip = 0;
+        GetHistoryQueryResult result;
+        do
+        {
+            var query = new GetCalculationHistoryQuery(
+                request.UserId,
+                take,
+                skip);
+
+            result = await _mediator.Send(query, context.CancellationToken);
+
+            foreach (var historyItem in result.Items)
+            {
+                await responseStream.WriteAsync(new GetHistoryResponse
+                {
+                    Cargo = CargoResponse.Create(historyItem.Volume, historyItem.Weight, historyItem.GoodIds),
+                    Price = DecimalValue.FromDecimal(historyItem.Price)
+                });
+            }
+
+            skip += take;
+        } while (result.Items.Length != 0);
     }
 }
